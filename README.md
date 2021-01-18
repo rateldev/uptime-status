d6672d2ba5007cecedc444a58ef55d49b9f936e5
ratel <350790090@qq.com>
2021-01-18 19:25:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # uptime-status
-基于yb大佬的uptime-status修改而来
+A uptime status dashboard based on UptimeRobot API
+## 原项目地址
+> https://github.com/yb/uptime-status
+
+基于yb大佬的uptime-status修改而来,调整了样式，修改了数据方向
---
+++ src/components/link.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Link = (props) => {
+  return (
+    <a {...props} href={props.to} target="_blank" rel="noopener noreferrer">
+      {props.text}
+    </a>
+  );
+};
+
+export default Link;
